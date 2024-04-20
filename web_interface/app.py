35bd3591f8f9dfe34ba5acab792355ed077b6b28
xiancong <familykoay@gmail.com>
2024-04-21 01:19:50 +0800

    update comments

--- web_interface/app.py
+++ web_interface/app.py
@@ -2,7 +2,6 @@ import serial
 import streamlit as st
 import plotly.graph_objects as go
 
-# Streamlit UI
 st.sidebar.title('Settings')
 
 # User input for baud rate and serial port in the sidebar
@@ -12,23 +11,18 @@ serial_port = st.sidebar.selectbox('Select Serial Port', options=['COM5', 'COM1'
 # Open the serial port
 ser = serial.Serial(serial_port, baud_rate)
 
-# Title for the main app
 st.title('Real-Time ECG Emotion Classification')
 
-# Main layout for ECG chart and prediction widget
 main_container = st.container()
 
-# Create plotly chart
 chart = st.plotly_chart(go.Figure(), use_container_width=True)
 
-# Initialize variables for prediction, model confidence, and duration
 ecg_values = []  # Define ecg_values as a global variable
 prediction_text = st.metric(label="Prediction", value="", delta=None)
 confidence_0_text = st.metric(label="Model Confidence 0", value="", delta=None)
 confidence_1_text = st.metric(label="Model Confidence 1", value="", delta=None)
 duration_text = st.metric(label="Duration (ms)", value="", delta=None)
 
-# Function to parse the serial data
 def parse_serial_data(data):
     global ecg_values
     data = data.decode().strip()
@@ -44,7 +38,6 @@ def parse_serial_data(data):
         prediction = int(prediction_info[0].strip()) if prediction_info[0].strip() != "Sensor Detached" else "Sensor Detached"
         duration = float(duration_line.split(':')[-1].strip())
 
-        # Update the prediction widget
         prediction_text.metric(label="Prediction", value="stress" if prediction == 1 else "non-stress" if prediction == 0 else prediction, delta=None)
         confidence_0_text.metric(label="Model Confidence (Non-stress)", value=confidence_0, delta=None)
         confidence_1_text.metric(label="Model Confidence (Stress)", value=confidence_1, delta=None)
@@ -54,7 +47,6 @@ def parse_serial_data(data):
         try:
             ecg_values.append(float(data))
             if len(ecg_values) >= 500:
-                # Update the chart with new ECG data and clear the stored data
                 chart.plotly_chart(go.Figure(), use_container_width=True)
                 ecg_values.clear()
             else:
@@ -74,5 +66,4 @@ def update_graph():
     except KeyboardInterrupt:
         ser.close()
 
-# Run the update graph function
 update_graph()
